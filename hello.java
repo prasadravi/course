157e81630ac44e2a681804f268818231a126411c
Ravi prasad <prasadravi9866@gmail.com>
2024-10-24 00:38:54 +0530

    hello java

--- hello.java
+++ hello.java
@@ -2,7 +2,21 @@ class Hello
 {
     public static void main(String a[])
     {
-        System.out.print("hello world");
-    }
+        System.out.println(3+8);  
+        System.out.println(4+8);  
+     }
+
+}
+
+
+class hello
+{
+    public static void main(String a[])
+    {
+        int num1 = 3;
+        int num2 = 2;
+        int result = num1 + num2;
+        System.out.println(result);  
+     }
 
 }
