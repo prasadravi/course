9f56796683cd6bf1423e8059cd6433dafc1b4b49
Ravi prasad <prasadravi9866@gmail.com>
2024-10-24 01:13:09 +0530

    hello world

--- hello.java
+++ hello.java
@@ -1,22 +1,18 @@
-class Hello
-{
-    public static void main(String a[])
-    {
-        System.out.println(3+8);  
-        System.out.println(4+8);  
-     }
-
-}
-
-
 class hello
 {
     public static void main(String a[])
     {
-        int num1 = 3;
-        int num2 = 2;
-        int result = num1 + num2;
-        System.out.println(result);  
+        int num = 2;
+        byte by = 127;
+        Short sh = 5845;
+        long l = 54456l;
+
+        float f = 5.4f;
+        double d = 5.6;
+
+        char c = 'k';
+
+        boolean b = true;
      }
 
 }
